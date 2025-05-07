166339b8efd309591e48189d7a3bcd69be1de6a5
Neo-TX <100090985+Clenilton-Filho@users.noreply.github.com>
2025-05-07 19:59:50 -0300

    update redefinir-senha page

--- js/redefinir-senha.js
+++ js/redefinir-senha.js
@@ -1,3 +1,12 @@
+function sideMenu(){
+    $sideMenu = document.getElementById('side-menu');
+
+    if ($sideMenu.style.left == '0%'){
+        $sideMenu.style.left = '-50%';
+    }else{
+        $sideMenu.style.left = '0%';
+    }
+}
 function mostrarSenha(event){
     const $botaoMostrar = event.target.closest(".mostrar-senha");
     const $senhaInput = $botaoMostrar.parentElement.querySelector(".senha-input");
