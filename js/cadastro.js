35037e9e78763272fdef4178ba36bf91e3cedf3b
Neo-TX <100090985+Clenilton-Filho@users.noreply.github.com>
2025-05-07 19:53:45 -0300

    restore side menu functionality in the portrait version and removed it from the landscape version

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,3 +1,25 @@
+function sideMenu(){
+    $sideMenu = document.getElementById('side-menu');
+
+    if ($sideMenu.style.left == '0%'){
+        $sideMenu.style.left = '-50%';
+    }else{
+        $sideMenu.style.left = '0%';
+    }
+}
+function mostrarSenha(){
+    const $senhaInput = document.getElementById("password-input");
+    const $mostrarSenhaIcone = document.getElementById("mostrar-senha")
+
+    if ($senhaInput.type == "password"){
+        $senhaInput.type = "text";
+        $mostrarSenhaIcone.style.opacity = "100%"
+    }else{
+        $senhaInput.type = "password";
+        $mostrarSenhaIcone.style.opacity = "50%"
+    }
+}
+
 function previewImagem(event) {
     const reader = new FileReader();
     const formIcon = document.getElementById("form-icon");
--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+function sideMenu(){
+    $sideMenu = document.getElementById('side-menu');
+
+    if ($sideMenu.style.left == '0%'){
+        $sideMenu.style.left = '-50%';
+    }else{
+        $sideMenu.style.left = '0%';
+    }
+}
 function mostrarSenha(){
     const $senhaInput = document.getElementById("password-input");
     const $mostrarSenhaIcone = document.getElementById("mostrar-senha")
