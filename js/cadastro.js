f2a920fa9c45729b01a36a5fc8370fde3ae1d673
Jonathan Barros <pvojonathan83@gmail.com>
2025-05-12 17:24:55 -0300

    Validação de senhas adicionada. A senha segue o modelo de ao menos 1 maiúscula, 5 caracteres e 1 número.

--- js/cadastro.js
+++ js/cadastro.js
@@ -19,7 +19,6 @@ function mostrarSenha(event){
         $mostrarSenhaIcone.style.opacity = "50%";
     }
 }
-
 function previewImagem(event) {
     const reader = new FileReader();
     const formIcon = document.getElementById("form-icon");
@@ -32,3 +31,64 @@ function previewImagem(event) {
     };
     reader.readAsDataURL(event.target.files[0]);
 }
+function errorMessage(mensagem) {
+    const errorCheck = document.querySelector(".error-message");
+    if (errorCheck){ //Checar se a mensagem já existe, para apagar.
+        errorCheck.remove();
+    }
+
+    const $errorElement = document.createElement("p");
+    $errorElement.classList.add("error-message");
+    $errorElement.innerHTML = `⚠️ ${mensagem}`;          
+    
+    //CSS da mensagem de erro
+    $errorElement.style.cssText = `
+        display: flex;
+        align-self: flex-end;
+        color: white;
+        font-size: 11px;
+        margin-bottom: -10px;
+    `;
+    
+    const $container = document.querySelector("#email-div");
+    $container.before($errorElement); 
+}
+document.addEventListener("DOMContentLoaded", () => {
+    const $emailInput = document.querySelector("#email-input");
+    const $senhaInput = document.querySelector("#password-input");
+    const $confirmarInput = document.querySelector("#confirmar-senha");
+    
+    $emailInput.setAttribute("required", "true");
+    $senhaInput.setAttribute("required", "true");
+    $confirmarInput.setAttribute("required", "true");
+    
+    //Validação do submit do form
+    const form = document.querySelector("#login-form");
+    form.addEventListener("submit", (event)=>{
+        event.preventDefault();
+        const haveNumber = new RegExp("[0-9]");
+        const haveUppercase = new RegExp("[A-Z]")
+
+        if ($confirmarInput.value != $senhaInput.value){ //Evitar senha diferente
+            errorMessage("As senhas não coincidem");
+            return;
+        }
+
+        if ($senhaInput.value.length < 5){ //Evitar senha muito curta.
+            errorMessage("A senha deve ter no mínimo 5 caracteres.");
+            return;
+        }
+        
+        if (!haveNumber.test($senhaInput.value)){ //Obrigar usar número.
+            errorMessage("A senha deve conter ao menos 1 número.");
+            return;
+        }
+
+        if (!haveUppercase.test($senhaInput.value)){ //Obrigar letra maiúscula
+            errorMessage("A senha deve conter uma letra maiúscula.");
+            return;
+        }
+
+        form.submit();
+    });
+});
