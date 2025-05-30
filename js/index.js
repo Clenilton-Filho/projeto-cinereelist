efdf76bc45a1f54ce426006937bfc8d1ae3ceabb
Neo-TX <100090985+Clenilton-Filho@users.noreply.github.com>
2025-05-30 16:47:31 -0300

    main page major overhaul

--- js/index.js
+++ js/index.js
@@ -1,10 +1,43 @@
 
 
-//deixar as animações de rolagem mais lentas
+
+
+function telaCheia(){
+    $trailer = document.getElementById("trailer-1");
+    $trailer.requestFullscreen();
+    $trailer.controls = true;
+    $trailer.muted = false;
+}
+
+document.addEventListener('fullscreenchange', () => {
+    $trailer = document.getElementById("trailer-1")
+    if (!document.fullscreenElement) {
+        $trailer.controls = false;
+        $trailer.muted = true;
+    }
+});
+
+//mudar os filmes ((((((teste))))))
+
+
+function mudarFilme(event){
+    $trailer = document.getElementById("trailer-1");
+    
+    if (event.target.src.includes("revenge_of_the_sith.jpg")){
+        if (!$trailer.src.includes("revenge_of_the_sith.mp4")){
+            $trailer.src = "media/videos/revenge_of_the_sith.mp4";
+        }
+    }else if (event.target.src.includes("the_matrix.jpg")){
+        if (!$trailer.src.includes("the_matrix_trailer_2.mp4")){
+            $trailer.src = "media/videos/the_matrix_trailer_2.mp4";
+        }
+    }
+}
 
 function rolarEsquerda(event){
     const $rolarDireita = document.getElementById('mudar-em-alta-direita');
     const $conteiner = document.getElementById("conteiner-capas-2-em-alta");
+    const curti = document.querySelectorAll(".curti");
     const quantidade = $conteiner.offsetWidth;
 
     //rola o contêiner para a esquerda pelo tamanho do contêiner
@@ -17,11 +50,17 @@ function rolarEsquerda(event){
     //habilitando o outro botão
     $rolarDireita.style.opacity = 1;
     $rolarDireita.style.pointerEvents = 'auto';
+
+    //reposicionando os botões curtir
+    curti.forEach($botao => {
+        $botao.style.marginRight = "auto";
+    });
 }
       
 function rolarDireita(event){
     const $rolarEsquerda = document.getElementById('mudar-em-alta-esquerda');
     const $conteiner = document.getElementById("conteiner-capas-2-em-alta");
+    const curti = document.querySelectorAll(".curti");
     const quantidade = $conteiner.offsetWidth;
 
     //rola o contêiner para a direita pelo tamanho do contêiner
@@ -34,6 +73,13 @@ function rolarDireita(event){
     //habilitanto o outro botão
     $rolarEsquerda.style.opacity = 1;
     $rolarEsquerda.style.pointerEvents = 'auto';
+
+    //reposicionando os botões de curtir
+    //reposicionando os botões curtir
+    curti.forEach($botao => {
+        $botao.style.marginRight = "66.5%";
+    });
+
 }
 
 /*
